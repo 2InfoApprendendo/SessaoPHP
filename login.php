ce41f2d04fd2f92a534d95ee3a8abd750ce84e8e
L-aur-a-svg <lgomesjesusdeoliveira@gmail.com>
2025-09-26 09:35:41 -0300

    Update login.php
    Laura Oliveira- 2A, GB, correção do cóigo

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
+<html lang="pt-BR">
 <head>
-    <title>Login Page</title>
     <meta charset="UTF-8">
+    <title>Login Page</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -9,11 +10,16 @@
         <div class="form">
             <h2>Login</h2>
             <form class="login-form" action="autenticacao.php" method="POST">
-                <input type="text" placeholder="login" name="login"/>
-                <input type="password" placeholder="senha" name="senha"/>
-                <button>login</button>
+                <label for="login">Login:</label>
+                <input type="text" id="login" name="login" placeholder="login" required>
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" placeholder="senha" required>
+
+                <button type="submit">Login</button>
                 <p class="message">Não foi cadastrado? <a href="#">Criar a conta</a></p>
             </form>
         </div>
     </div>
 </body>
+</html> 
